98f258128ae161161a0109a609f5aaa3334bfead
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2016-06-16 22:44:07 +0300

    Added file writer

--- src/main/java/diploma/producer/Main.java
+++ src/main/java/diploma/producer/Main.java
@@ -8,6 +8,9 @@ import twitter4j.Status;
 import twitter4j.TwitterException;
 import twitter4j.TwitterObjectFactory;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 import java.util.Random;
 
@@ -15,7 +18,7 @@ import java.util.Random;
  * Created by Никита on 21.04.2016.
  */
 public class Main {
-    public static void main(String[] args) throws TwitterException, InterruptedException {
+    public static void main(String[] args) throws TwitterException, InterruptedException, FileNotFoundException, UnsupportedEncodingException {
         Properties props = new Properties();
         props.put("bootstrap.servers", "192.168.1.23:9092, 192.168.1.22:9092");
         props.put("acks", "all");
@@ -26,7 +29,7 @@ public class Main {
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-        Producer<String, String> producer = new KafkaProducer<>(props);
+        //Producer<String, String> producer = new KafkaProducer<>(props);
 
         TwitterStreamConnection.getInstance("YOcgp2ovL8js849lx8hbnvxcf",
                 "IUxjGCksxWJiBlQ5PMsp5O8ksT7ZAsTDspOQafm46gSkYnII4u",
@@ -34,6 +37,8 @@ public class Main {
                 "1nCVck1dtozb334vxlyca9Wb3Gq5ob7USXEX5sIqmIugs").getClient().connect();
 
         Random random = new Random();
+        PrintWriter jsonWriter = new PrintWriter("~/diploma/tweets/json-tweets.txt", "UTF-8");
+        //PrintWriter textWriter = new PrintWriter("~/diploma/tweets/text-tweets.txt", "UTF-8");
         int i = 0;
         while (true) {
             if (TwitterStreamConnection.getInstance().getClient().isDone()) {
@@ -46,8 +51,9 @@ public class Main {
                 System.out.println("Did not receive a message in 1 second");
             } else {
                 i++;
-                producer.send(new ProducerRecord<>("my-replicated-topic", Integer.toString(i), msg));
-                if (i == 100000)
+                jsonWriter.write(msg);
+                //producer.send(new ProducerRecord<>("my-replicated-topic", Integer.toString(i), msg));
+                if (i == 500000)
                     break;
 //                try {
 //                    Status status = TwitterObjectFactory.createStatus(msg);
@@ -58,6 +64,8 @@ public class Main {
 //                }
             }
         }
+
+        jsonWriter.close();
 
         //for(int i = 0; i < 500; i++) {
             //producer.send(new ProducerRecord<>("my-replicated-topic", Integer.toString(i), "{\"text\": \"status" + i+ "\"}"));
